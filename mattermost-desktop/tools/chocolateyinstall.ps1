189a82dc51bf04878d28ed9855102fd87cf4c0f5
hansnull <t-len@gmx.de>
2018-04-04 09:34:26 +0200

    Update to 4.0.1 and fixes links

--- mattermost-desktop/tools/chocolateyinstall.ps1
+++ mattermost-desktop/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'mattermost-desktop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://releases.mattermost.com/desktop/4.0.0/mattermost-setup-4.0.0-win32.exe'
-$url64      = 'https://releases.mattermost.com/desktop/4.0.0/mattermost-setup-4.0.0-win64.exe'
+$url        = 'https://releases.mattermost.com/desktop/4.0.1/mattermost-setup-4.0.1-win32.exe'
+$url64      = 'https://releases.mattermost.com/desktop/4.0.1/mattermost-setup-4.0.1-win64.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -18,9 +18,9 @@ $packageArgs = @{
   silentArgs	= '--silent'
 
   softwareName  = 'Mattermost*'
-  checksum      = 'e8a25713fcc84d3acde1a982fd5a464dcd09f3655c3f0bb3d2b39146e6cbf719'
+  checksum      = 'cfabd2c92fc0618dbe31952f0715d0708da7240a94a0c20f3e12aa95e8a7467c'
   checksumType  = 'sha256'
-  checksum64    = 'afc699a4bf79fb6ddb94f6c1777eb4e930c0126f4c9d39238e22f63a43cf4d02'
+  checksum64    = 'ccb465509844dc0a6cf4035212854e2fc477c23e32321f82a936db861d73f5cb'
   checksumType64= 'sha256'
 }
 
